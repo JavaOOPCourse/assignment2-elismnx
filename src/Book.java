ef4e0096daea96f86a7906b168f188464ea89ca4
elis <elissnnn8@gmail.com>
2026-02-09 18:09:15 +0600

    .

--- src/Book.java
+++ src/Book.java
@@ -10,6 +10,7 @@ public class Book {
     public Book(String title, String author, int year) {
         this.title = title;
         this.author = author;
+        this.year = year;
     }
 
     // TODO: Implement copy constructor
